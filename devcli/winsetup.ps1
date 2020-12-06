e7cba3f58f0adc88c959f29726cd5aabada61e51
Javier Matias-Cabrera <javier.l.matias@gmail.com>
2020-12-05 19:24:50 -0500

    Added windows setup script.

---
+++ devcli/winsetup.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+choco install -y vscode pwsh vcxsrv
+scoop install starship
