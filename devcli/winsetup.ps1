9fbeea9db5a19c323566dc77d9b430aa3e092430
Javier L. Matías-Cabrera <javier.l.matias@gmail.com>
2020-12-05 19:59:38 -0500

    Update winsetup.ps1
    Former-commit-id: a295f93fee80add892c582b6c648221ca7edf2b6
    Former-commit-id: f7ee293fa3f0206d134fb7faacca157d6923fcce

--- devcli/winsetup.ps1
+++ devcli/winsetup.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 
-choco install git.install --params "/NoGitLfs"
+choco install git.install --params "/NoGitLfs" -y
 choco install -y vscode pwsh vcxsrv jetbrainsmono
 scoop install starship
 
