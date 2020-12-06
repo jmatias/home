dba2bc31de9b6748f3cebc94d6466d06a4a2bb1c
Javier Matias-Cabrera <javier.l.matias@gmail.com>
2020-12-05 19:52:09 -0500

    Install wsl

--- devcli/winsetup.ps1
+++ devcli/winsetup.ps1
@@ -1,5 +1,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 
-choco install -y vscode pwsh vcxsrv
+choco install git.install --params "/NoGitLfs"
+choco install -y vscode pwsh vcxsrv jetbrainsmono
 scoop install starship
+
+wsl --install -d Ubuntu
